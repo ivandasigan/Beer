98129c1acf56aaf380d65c64b2cf30fdf2c76372
ivandasigan <ivandasiganofficial@gmail.com>
2021-12-21 23:12:07 +0800

    Added brand model

--- beers/models.py
+++ beers/models.py
@@ -9,3 +9,8 @@ class Beer(models.Model):
     srp = models.FloatField()
     
 class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    beers = models.ManyToManyField(Beer)
+    def __str__(self):
+        return self.name
