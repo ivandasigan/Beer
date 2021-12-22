22aedad9937745cbc0334e0014c36c0d85810c4d
ivandasigan <ivandasiganofficial@gmail.com>
2021-12-22 09:18:39 +0800

    modified serializer

--- beers/serializer.py
+++ beers/serializer.py
@@ -1,1 +1,14 @@
 
+from rest_framework import fields, serializers
+from .models import Beer, Brand
+
+class BeerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Beer
+        fields = ('id','name','size','srp')
+
+class BrandSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ('id','name','beers')
+
