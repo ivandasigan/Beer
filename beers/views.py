589d68e4ab138f7af995031218a458c8abf772c4
ivandasigan <ivandasiganofficial@gmail.com>
2021-12-23 09:48:57 +0800

    Modified views

--- beers/views.py
+++ beers/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from beers.models import Beer, Brand
+from .serializer import BeerSerializer, BrandSerializer
+
 
 # Create your views here.
+
+class BeerView(viewsets.ModelViewSet):
+    queryset = Beer.objects.all()
+    serializer_class = BeerSerializer
