7485c4760e8a2f3cb9f556500cc1a7ab913b335d
ivandasigan <51710349+ivandasigan@users.noreply.github.com>
2021-12-26 10:50:31 +0800

    modified models and urls

--- beers/urls.py
+++ beers/urls.py
@@ -1,2 +1,12 @@
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework import urlpatterns
+from rest_framework.routers import DefaultRouter
+from .views import BeerView, BrandView
+router = DefaultRouter()
+router.register('beers', BeerView)
+router.register('brand', BrandView)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
